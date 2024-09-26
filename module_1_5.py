3ed9faf66faadbf8f1fff38b8d396b8e4b9889ab
Aleksandr0917 <180825344+Aleksandr0917@users.noreply.github.com>
2024-09-26 08:43:14 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var=1, 2, 3,"box"
+print(immutable_var)
+#immutable_var[2]=6
+print(immutable_var)
+
+mutable_list=(11, 23, 'kite', 'home')
+mutable_list=[11, 23, True, 'kite',[12, 45]]
+print(mutable_list)
